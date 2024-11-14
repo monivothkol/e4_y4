3d1f85cc07ac52427c958476a9be4ef908283dae
MacBook <monivothkol1999@gmail.com>
2024-11-14 13:44:11 +0700

    emailjs

--- src/script/emailjs.js
+++ src/script/emailjs.js
@@ -4,6 +4,7 @@ function sendMail(){
         sender_phone : document.getElementById("sender_phone").value,
         sender_email: document.getElementById("sender_email").value,
         sender_subject : document.getElementById("sender_subject").value,
+        sender_message : document.getElementById("sender_message"),
     }
     emailjs.send("service_mpytdrf", "template_paqig5h", param).then(alert("Your email was sent successfully. We will contact you soon!"))
     console.log("send success")
