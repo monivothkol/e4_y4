6ceefd021eab21edb1e604e43c3dae29db1333e7
MacBook <monivothkol1999@gmail.com>
2024-12-07 10:47:33 +0700

    fix path

--- src/script/header-footer.js
+++ src/script/header-footer.js
@@ -1,7 +1,7 @@
 var path_logo = "../assets/images/logo.png"
 var link = {
-  home : "../index.html",
-  network : "/*e4_y4*//src/html/networking.html",
+  home : "/index.html",
+  network : "/src/html/networking.html",
   storage : "/src/html/Storage.html",
   personalUse : "/src/html/personal_service.html",
   smallBusiness : "",
