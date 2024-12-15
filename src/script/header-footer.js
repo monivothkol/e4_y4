1bf7956775e4fae86457194e049d303511d64184
naknak1234 <naknak0hp@gmail.com>
2024-12-15 11:43:42 +0700

    sign in and index update

--- src/script/header-footer.js
+++ src/script/header-footer.js
@@ -48,11 +48,11 @@ document.addEventListener("DOMContentLoaded", function() {
         </nav>
         <div class="header-icons">
             <div class="header-icons">
-                <a href="cart.html" class="cart-icon-parent"><svg xmlns="http://www.w3.org/2000/svg" class="cart-icon" viewBox="0 0 16 16">
+                <a href="/src/html/cart.html" class="cart-icon-parent"><svg xmlns="http://www.w3.org/2000/svg" class="cart-icon" viewBox="0 0 16 16">
                   <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
                   </svg>
                 </a>
-                <a href="sign-in.html" class="sign-in" data-replace="Sign In"><span>Sign In</span></a>
+                <a href="/src/html/sign-in.html" class="sign-in" data-replace="Sign In"><span>Sign In</span></a>
             </div>
             <div class="menu-toggle" onclick="toggleMenu()">
                 <span></span>
