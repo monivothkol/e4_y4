ebed629bcc1b95cd2849e6c36a673dace05640c0
vathana.ph <vathanapheng2@gmail.com>
2024-12-15 12:11:11 +0700

    fixed

--- src/script/emailjs.js
+++ src/script/emailjs.js
@@ -1,11 +1,22 @@
-function sendMail(){
+function sendMail() {
     let param = {
-        sender_name : document.getElementById("sender_name").value,
-        sender_phone : document.getElementById("sender_phone").value,
+        sender_name: document.getElementById("sender_name").value,
+        sender_phone: document.getElementById("sender_phone").value,
         sender_email: document.getElementById("sender_email").value,
-        sender_subject : document.getElementById("sender_subject").value,
-        sender_message : document.getElementById("sender_message"),
+        sender_subject: document.getElementById("sender_subject").value,
+        sender_message: document.getElementById("sender_message"),
     }
     emailjs.send("service_mpytdrf", "template_paqig5h", param).then(alert("Your email was sent successfully. We will contact you soon!"))
     console.log("send success")
+}
+
+
+function contactUs() {
+    alert("Contact us at support@networksolutions.com");
+}
+
+function showDetails(option) {
+    const sections = document.querySelectorAll('.services');
+    sections.forEach(section => section.classList.add('hidden'));
+    document.getElementById(option).classList.remove('hidden');
 }
