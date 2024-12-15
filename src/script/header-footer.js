7c9489252ad7cabd70ed0d0fc7d23ce77daeb9fd
vathana.ph <vathanapheng2@gmail.com>
2024-12-15 11:40:58 +0700

    fixed

--- src/script/header-footer.js
+++ src/script/header-footer.js
@@ -1,15 +1,16 @@
 var path_logo = "../assets/images/logo.png"
 var link = {
-  home : "/index.html",
-  network : "/src/html/networking.html",
-  storage : "/src/html/Storage.html",
-  personalUse : "/src/html/personal_service.html",
-  smallBusiness : "",
-  enterprise : "",
-  ourCompany : "",
-  ourPartner : "./src/html/partner.html",
-  ourClient : "./src/html/client.html",
-  contact : "./src/html/contact.html",
+    home: "/index.html",
+    network: "/src/html/networking.html",
+    storage: "/src/html/Storage.html",
+    personalUse: "/src/html/personal_service.html",
+    smallBusiness: "",
+    enterprise: "",
+    ourCompany: "",
+    ourPartner: "./src/html/partner.html",
+    ourClient: "./src/html/client.html",
+    contact: "./src/html/contact.html",
+    cart: "../html/cart.html",
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -48,7 +49,7 @@ document.addEventListener("DOMContentLoaded", function() {
         </nav>
         <div class="header-icons">
             <div class="header-icons">
-                <a href="cart.html" class="cart-icon-parent"><svg xmlns="http://www.w3.org/2000/svg" class="cart-icon" viewBox="0 0 16 16">
+                <a href="${link.cart}" class="cart-icon-parent"><svg xmlns="http://www.w3.org/2000/svg" class="cart-icon" viewBox="0 0 16 16">
                   <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
                   </svg>
                 </a>
